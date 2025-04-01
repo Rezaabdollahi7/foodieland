404030657a4d5ec40162c6563844647cff43b833
Rezaabdollahi7 <srezaabdollahi7@gmail.com>
2025-04-02 01:11:32 +0330

    feat: enhance CookingWithUs section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Navbar from '@/components/Navbar';
 import Categories from './Sections/Categories';
+import CookWithUs from './Sections/CookWithUs';
 export default function Home() {
   return (
     <>
       <Navbar />
       <Categories />
+      <CookWithUs />
     </>
   );
 }
